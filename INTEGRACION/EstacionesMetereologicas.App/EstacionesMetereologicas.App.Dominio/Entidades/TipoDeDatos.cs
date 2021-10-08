9fd190518d49f776a45dac085ce69c27055590b0
Stiven Atis <stivenn06@gmail.com>
2021-10-07 21:05:28 -0500

    Correción de errores al conectar a la base de datos

---
+++ INTEGRACION/EstacionesMetereologicas.App/EstacionesMetereologicas.App.Dominio/Entidades/TipoDeDatos.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EstacionesMetereologicas.App.Dominio
+{
+    public class TipoDeDatos
+    {
+        public int Id { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public string Estado { get; set; }
+    }
+}
